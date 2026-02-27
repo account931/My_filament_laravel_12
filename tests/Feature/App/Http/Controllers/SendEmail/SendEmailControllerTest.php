7a0e71991c60305690a2f7de4168403eb2c3ce7e
Account931 <account931@ukr.net>
2026-02-27 18:10:30 +0100

    Email test fix

--- tests/Feature/App/Http/Controllers/SendEmail/SendEmailControllerTest.php
+++ tests/Feature/App/Http/Controllers/SendEmail/SendEmailControllerTest.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\SendEmail\SendEmailController;
+use App\Jobs\Mail\SendMail;
 use App\Mail\CustomEmail;
-use App\Models\User;
 // optional, to get correct namespace
+use App\Models\User;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Queue;
 
 use function Pest\Laravel\get;
 
@@ -33,6 +35,8 @@ it('validates the email and message fields', function () {
         ->assertSessionHasErrors(['email', 'message']);
 });
 
+// it covers only variant with Mail::to($data['email'])->queue(new CustomEmail($data['email'], $data['message'])); but it has been changed to JOB
+/*
 it('queues a CustomEmail when valid data is sent', function () {
     $user = User::factory()->create();
 
@@ -48,5 +52,28 @@ it('queues a CustomEmail when valid data is sent', function () {
 
     Mail::assertQueued(CustomEmail::class, function ($mail) use ($data) {
         return $mail->hasTo($data['email']);
+    });
+});
+*/
+
+// variant for SendMail::dispatch($data['email'], $data['message']);
+it('dispatches the SendMail job when valid data is sent', function () {
+    Queue::fake();
+
+    $user = User::factory()->create();
+
+    $data = [
+        'email' => 'user@example.com',
+        'message' => 'Hello world!',
+    ];
+
+    $this->actingAs($user)
+        ->post(action([SendEmailController::class, 'handleSendEmail']), $data)
+        ->assertRedirect()
+        ->assertSessionHas('flashSuccess', 'Your Mail Facade letter was sent successfully to user '.$data['email']);
+
+    Queue::assertPushed(SendMail::class, function ($job) use ($data) {
+        return $job->email === $data['email'] &&
+               $job->message === $data['message'];
     });
 });
